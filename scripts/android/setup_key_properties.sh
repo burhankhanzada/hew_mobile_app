e8a75a732848dfac688605d9a4d33259fb7a1afd
Burha Khanzada <burhankhanzada199888@gmail.com>
2022-08-21 13:47:40 +0500

    ci: Remove unnessary code

--- scripts/android/setup_key_properties.sh
+++ scripts/android/setup_key_properties.sh
@@ -1,12 +1,5 @@
 #!/bin/env bash
 
-echo "Create keystore file from base64 format to $CM_KEYSTORE_PATH"
-
-echo $CM_KEYSTORE | base64 --decode >$CM_KEYSTORE_PATH
-
-keytool -list -v -keystore $CM_KEYSTORE_PATH \
-    -storepass $CM_KEYSTORE_PASSWORD -$alias CM_KEY_ALIAS
-
 KEY_PROPERTIES_PATH="$CM_BUILD_DIR/android/key.properties"
 
 cat >>$KEY_PROPERTIES_PATH <<EOF
